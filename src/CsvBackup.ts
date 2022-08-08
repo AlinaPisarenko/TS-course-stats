d283e851545de60c0e50ea1cdc94f59dd4de6e33
Alina <pisarenko.alina@gmail.com>
2022-08-07 22:33:52 -0400

    test

--- src/CsvBackup.ts
+++ src/CsvBackup.ts
@@ -1,35 +1,35 @@
-import fs from 'fs';
-import { dateStringToDate } from './utils';
-import { MatchResult } from './MatchResult';
+// import fs from 'fs';
+// import { dateStringToDate } from './utils';
+// import { MatchResult } from './MatchResult';
 
-type MatchData = [Date, string, string, number, number, MatchResult, string];
+// type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-export class CsvFileReader {
-  data: MatchData[] = [];
+// export class CsvFileReader {
+//   data: MatchData[] = [];
 
-  constructor(public filename: string) {}
+//   constructor(public filename: string) {}
 
-  read(): void {
-    this.data = fs
-      .readFileSync(this.filename, {
-        encoding: 'utf-8',
-      })
-      .split('\n')
-      .map((row: string): string[] => {
-        return row.split(',');
-      })
-      .map(this.mapRow);
-  }
+//   read(): void {
+//     this.data = fs
+//       .readFileSync(this.filename, {
+//         encoding: 'utf-8',
+//       })
+//       .split('\n')
+//       .map((row: string): string[] => {
+//         return row.split(',');
+//       })
+//       .map(this.mapRow);
+//   }
 
-  mapRow(row: string[]): MatchData {
-    return [
-      dateStringToDate(row[0]),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      row[5] as MatchResult,
-      row[6],
-    ];
-  }
-}
+//   mapRow(row: string[]): MatchData {
+//     return [
+//       dateStringToDate(row[0]),
+//       row[1],
+//       row[2],
+//       parseInt(row[3]),
+//       parseInt(row[4]),
+//       row[5] as MatchResult,
+//       row[6],
+//     ];
+//   }
+// }
